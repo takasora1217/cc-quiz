6aaaca72a17919104d5a6b947e3d03f970b032b3
sahono6363 <mumi6363n@gmail.com>
2025-08-25 01:02:09 +0900

    ホームページのデザイン修正

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -7,11 +7,10 @@ function HomePage() {
 
   return (
     <div className="HomePage">
-      <h1>home</h1>
       <img src="/img/リーーーグ.png" />
       <div className="buttons">
         <button onClick={() => navigate("/play")}>遊ぶ</button>
-        <button onClick={() => navigate("/create")}>作る</button>
+        <button onClick={() => navigate("/create-question")}>作る</button>
       </div>
     </div>
   );
