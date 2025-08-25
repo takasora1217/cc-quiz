db2032c00020652350044f42e454449034a0679f
sahono6363 <mumi6363n@gmail.com>
2025-08-26 08:21:46 +0900

    クイズ作成ページ途中

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -8,9 +8,10 @@ function HomePage() {
   return (
     <div className="HomePage">
       <img src="/img/リーーーグ.png" />
+      <h1>みんなで作ってみんなで遊べるクイズゲーム</h1>
       <div className="buttons">
-        <button onClick={() => navigate("/play")}>遊ぶ</button>
         <button onClick={() => navigate("/create-question")}>作る</button>
+        <button onClick={() => navigate("/play")}>遊ぶ</button>
       </div>
     </div>
   );
